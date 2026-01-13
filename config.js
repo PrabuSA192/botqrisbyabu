96ba2cf99c10ebb11409a973ac1a125b424ea044
Prabu Setyo A <prabusetyo57@gmail.com>
2026-01-13 22:04:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  BOT_TOKEN: '8337735658:AAHyZU71AFe_WcXNCco7YM53OelUb6GvI8Y',
-  PAKASIR_PROJECT: 'telebotabuzy',
-  PAKASIR_API_KEY: 'fqeVS6M95IvE1H9twKWIiIBVH9PFYq9n',
-  OWNER_ID: '6250933363',
+  BOT_TOKEN: '', //Bot token telegram lu
+  PAKASIR_PROJECT: '', //Nama Project Pakasir Lu
+  PAKASIR_API_KEY: '', //Apikey Project Pakasir lu
+  OWNER_ID: '', //id owner
   // true = mode dev (auto sukses)
   // false = mode real (cek status Pakasir)
   DEV_MODE: false
 };
+
